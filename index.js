a9834737c1275d00f5895099c2fd1cda64045705
Gui <guilhermenen15@gmail.com>
2023-01-19 11:51:43 -0300

    update text files and formate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function loadBook(fileName, displayName) {
+function loadBook(filename, displayName) {
   let currentBook = "";
-  let url = "books/" + fileName;
+  let url = "books/" + filename;
 
   //reset our UI
   document.getElementById("fileName").innerHTML = displayName;
@@ -12,15 +12,19 @@ function loadBook(fileName, displayName) {
   xhr.open("GET", url, true);
   xhr.send();
 
-  xhr.onreadystatechange = function() {
-    if (readyState == 4 && xhr.status == 200) {
-      currentBook = xhr.responseText;
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          currentBook = xhr.responseText;
 
-      document.getElementById("fileContent").innerHTML = currentBook;
-      
-      // Scroll back to top when book is change
-      var element = document.getElementById("fileContent");
-      element.scrollTop = 0;
-    }
-  }
+          //getDocStats(currentBook);
+
+          //remove line breaks and carriage returns and replace with a <br>
+          currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
+          document.getElementById("fileContent").innerHTML = currentBook;
+
+          var elmnt = document.getElementById("fileContent");
+          elmnt.scrollTop = 0;
+      }
+  };
 }
